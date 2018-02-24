250ec91bf14ca544b43752db1b6857b913a124ba
tatdat <tatdat.to@gmail.com>
2018-02-24 12:20:01 +0100

    Add constant b

--- hellow_tf.py
+++ hellow_tf.py
@@ -2,7 +2,9 @@ import tensorflow as tf
 
 with tf.device("/gpu:0"):
     a = tf.constant([1.,2.])
+    b = tf.constant([3.,4.])
 
 with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
     print(session.run(a))
+    print(session.run(b))
 
