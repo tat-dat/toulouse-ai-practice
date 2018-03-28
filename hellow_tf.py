56ef978884685344b34ffc9261b81e3713ea8e69
ntzung <ntzung@gmail.com>
2018-03-28 16:22:06 +0200

    Test step by step debugging

--- hellow_tf.py
+++ hellow_tf.py
@@ -1,10 +1,5 @@
-import tensorflow as tf
+# test step debug
 
-with tf.device("/gpu:0"):
-    a = tf.constant([1.,2.])
-    b = tf.constant([3.,4.])
-
-with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
-    print(session.run(a))
-    print(session.run(b))
-
+a = 1
+b= 2
+c= a+b
