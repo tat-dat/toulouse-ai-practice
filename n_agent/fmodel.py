964d1a2d9b38d95efb662f56f4d5fa93ce9ac827
Hangntt <tthnguye@laas.fr>
2018-03-30 16:02:53 +0200

    sua ham V L mu

--- n_agent/fmodel.py
+++ n_agent/fmodel.py
@@ -8,7 +8,7 @@ Created on Tue Mar 27 14:49:15 2018
 import keras.backend as K
 from keras import layers
 from keras.models import Model
-from keras.layers import Input,core, concatenate, Conv2D, MaxPooling2D, Activation, UpSampling2D, BatchNormalization
+from keras.layers import Lambda, Input,core, concatenate, Conv2D, MaxPooling2D, Activation, UpSampling2D, BatchNormalization
 from keras.optimizers import RMSprop
 import gym
 import tensorflow as tf
@@ -24,10 +24,11 @@ from rl.core import Processor
 #to slice tensor x, x[1,a:b]: i.e. slice at place 1, from a to b. if the first place is
 #: then slice all place from a to b.
 def Lmodel(N):
-    a = Input(batch_shape=(1,N))
-    ob= Input(batch_shape=(1,N))
-    ain=K.transpose(a)
-    obin=K.transpose(ob)
+    a = Input(shape=(N,))
+    ob= Input(shape=(1,N))
+    transpose_layers = Lambda(lambda v: K.transpose(v))
+    ain=transpose_layers(a)
+    obin=transpose_layers(ob)
     x=concatenate([ain,obin])
     x=Dense(12)(x)
     x=Activation('relu')(x)
@@ -35,33 +36,38 @@ def Lmodel(N):
     x = Activation('relu')(x)
     x = Dense(32)(x)
     x = Activation('relu')(x)
-    x=K.transpose(x)
+    x=Dense((N*N+N)//2)(x)
+    x=transpose_layers(x)
     model=Model(inputs=[a,ob],outputs=x)
     return model
 #    print(model.summary())
 
 def Vmodel(N):
-    inpp=Input(batch_shape=(1,N))
-    inp=K.transpose(inpp)
+    inpp=Input(shape=(1,N))
+    transpose_layers = Lambda(lambda v: K.transpose(v))
+    inp=transpose_layers(inpp)
     x=Dense(12)(inp)
     x=Activation('relu')(x)
     x = Dense(32)(x)
     x = Activation('relu')(x)
     x = Dense(32)(x)
     x = Activation('relu')(x)
-    out=K.transpose(x)
+    x=Dense(N)(x)
+    out=transpose_layers(x)
     model=Model(inputs=inpp,outputs=out)
     return model
 
 def mumodel(N):
-    inpp=Input(batch_shape=(1,N))
-    inp=K.transpose(inpp)
+    inpp=Input(shape=(1,N))
+    transpose_layers = Lambda(lambda v: K.transpose(v))
+    inp=transpose_layers(inpp)
     x=Dense(12)(inp)
     x=Activation('relu')(x)
     x = Dense(32)(x)
     x = Activation('relu')(x)
     x = Dense(32)(x)
     x = Activation('relu')(x)
-    out=K.transpose(x)
+    x=Dense(N)(x)
+    out=transpose_layers(x)
     model=Model(inputs=inpp,outputs=out)
     return model
