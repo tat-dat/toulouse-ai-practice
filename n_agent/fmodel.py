47b051659add0976d33a59fdf5b189b7df9955a3
Hangntt <tthnguye@laas.fr>
2018-04-13 14:16:19 +0200

    correct dim

--- n_agent/fmodel.py
+++ n_agent/fmodel.py
@@ -24,13 +24,16 @@ from rl.core import Processor
 #to slice tensor x, x[1,a:b]: i.e. slice at place 1, from a to b. if the first place is
 #: then slice all place from a to b.
 def Lmodel(N):
-    a = Input(batch_shape=(N,))
-    ob= Input(batch_shape=(1,N))
-    expand_dims = Lambda(lambda v: K.expand_dims(v))
-    transpose_layers = Lambda(lambda v: K.transpose(v))
-    ain=transpose_layers(a)
+    a = Input(shape=(N,))
+    ob= Input(shape=(1,N))
+    expand_dims = Lambda(lambda v: K.expand_dims(v,axis=0))
+    expand_dim = Lambda(lambda v: K.expand_dims(v[0]))
+    transpose_layers = Lambda(lambda v: K.transpose(v[0]))
+#    ain=transpose_layers(a)
     obin=transpose_layers(ob)
-    ain = expand_dims(a)
+    obin=expand_dims(obin)
+    ain = expand_dim(a)
+    ain = expand_dims(ain)
     x=concatenate([ain,obin])
 #    x=concatenate([a,ob])
     x=Dense(4)(x)
@@ -39,17 +42,20 @@ def Lmodel(N):
     x = Activation('relu')(x)
     x = Dense(2)(x)
     x = Activation('relu')(x)
-    x=Dense(1)(x)
+    x=Dense((N*N+N)//2)(x)
     x=transpose_layers(x)
+#    x=expand_dims(x)
     model=Model(inputs=[a,ob],outputs=x)
     return model
 #    print(model.summary())
 
 def Vmodel(N):
-    inpp=Input(batch_shape=(1,N))
+    inpp=Input(shape=(1,N))
 #    inpp=Input(batch_shape=(1,N))
-    transpose_layers = Lambda(lambda v: K.transpose(v))
+    transpose_layers = Lambda(lambda v: K.transpose(v[0]))
     inp=transpose_layers(inpp)
+    expand_dims = Lambda(lambda v: K.expand_dims(v,axis=0))
+    inp=expand_dims(inp)
     x=Dense(2)(inp)
     x=Activation('relu')(x)
     x = Dense(2)(x)
@@ -58,13 +64,16 @@ def Vmodel(N):
     x = Activation('relu')(x)
     x=Dense(1)(x)
     x=transpose_layers(x)
+#    x=expand_dims(x)
     model=Model(inputs=inpp,outputs=x)
     return model
 
 def mumodel(N):
-    inpp=Input(batch_shape=(1,N))
-    transpose_layers = Lambda(lambda v: K.transpose(v))
+    inpp=Input(shape=(1,N))
+    transpose_layers = Lambda(lambda v: K.transpose(v[0]))
     inp=transpose_layers(inpp)
+    expand_dims = Lambda(lambda v: K.expand_dims(v,axis=0))
+    inp=expand_dims(inp)
     x=Dense(2)(inp)
     x=Activation('relu')(x)
     x = Dense(2)(x)
@@ -73,5 +82,6 @@ def mumodel(N):
     x = Activation('relu')(x)
     x=Dense(1)(x)
     out=transpose_layers(x)
+#    x=expand_dims(x)
     model=Model(inputs=inpp,outputs=out)
     return model
